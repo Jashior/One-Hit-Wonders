352c4f3f39e2fad383899342053adbf1d035e59b
saintjuniper <saintjuniper73@gmail.com>
2022-03-22 04:06:21 +0000

    theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#A3A3A3",
+        bgc: "#D4D4D4",
+        ctext: "23272A",
+        "bg-dark": "#171717",
+        "bgc-dark": "#262626",
+        "ctext-dark": "DEE2E7",
+      },
+    },
   },
   variants: {
     extend: {},
